1a29c207a44d8c63e2c0b36ee96d7229eedb5e23
Bilgihan Kose <bilgihankose@gmail.com>
2022-04-16 16:32:30 +0300

    added click event

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -5,6 +5,25 @@ import people from "../data";
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      if (index >= people.length - 1) {
+        return 0;
+      }
+      return index + 1;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      if (index < 1) {
+        return people.length - 1;
+      }
+      let newIndex = index - 1;
+      return newIndex;
+    });
+  };
   return (
     <article className="review">
       <div className="img-container">
@@ -17,10 +36,10 @@ const Review = () => {
       <p className="job tt-capitalize">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="next-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="prev-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
